56877b9725f4af624cbedc419fbe0ec9cf20c3cf
Julia Vorcakova <julia.vorcakova1@gmail.com>
2019-02-07 21:16:26 +0100

    CH5-averageTip

--- scripts.js
+++ scripts.js
@@ -378,3 +378,80 @@ var John = {
 
 John.calcTip();
 console.log(John);
+
+var Mark = {
+    firstName: 'Mark',
+    lastName: 'Perk',
+    bills: [77, 375,110,45],
+    calcTip: function() {
+        this.tips = [];
+        this.finalValue = [];
+
+        for (var i = 0; i < this.bills.length; i++) {
+
+            var percentage;
+            var bill = this.bills[i];
+
+            if(bill < 100) {
+                percentage = 0.2;
+            } else if( bill >= 100 && bill < 300){
+                percentage = 0.1;
+            } else{
+                percentage = 0.25;
+            }
+
+            this.tips[i] = bill * percentage;
+            this.finalValue[i] = bill + this.tips[i];
+        }
+    }   
+}
+
+
+Mark.calcTip();
+console.log(Mark);
+
+// avarageTip
+
+function averageTip(tips){
+    var sum  = 0;
+
+    for (var i = 0; i < tips.length; i++) {
+        sum = sum + tips[i];
+    }
+
+    average = sum / tips.length;
+
+    return average;
+}
+
+var tips = Mark.tips;
+var averageMark = averageTip(tips);
+console.log(average);
+
+var tips = John.tips;
+var averageJohn = averageTip(tips);
+console.log(average);
+
+if (averageMark > averageJohn){
+    console.log( 'Mark´s family gave in average more tips than John´s family. They gave ' + averageMark + ' € in average.')
+} else {
+    console.log( 'John´s family gave in average more tips than Mark´s family. They gave ' + averageJohn + ' € in average.') 
+}
+
+//Maxtip
+
+var tips = Mark.tips;
+function maxTip(tips) { 
+    var max = tips[0];
+    
+    for (var i = 0; i < tips.length; i++) {
+        if ( max < tips[i]) {
+          max = tips[i];
+        }
+    }
+    return max;  
+}
+
+var tips = Mark.tips;
+var max = maxTip(tips);
+console.log(max);   
