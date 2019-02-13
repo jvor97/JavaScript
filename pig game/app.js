a0d978010fb1073fe42315d86f4bc349b010a86a
Julia Vorcakova <julia.vorcakova1@gmail.com>
2019-02-13 22:16:22 +0100

    Random number, dice images, zero start scores

--- pig game/app.js
+++ pig game/app.js
@@ -15,15 +15,32 @@ var scores, roundScore, activePlayer, dice;
     roundScore = 0;
     activePlayer = 0;
 
-    dice = Math.floor(Math.random() * 6) + 1; //+1 tam je pretože chceš nie čísla od 0-5 ale od 1-6
-
-    document.querySelector('#current-' + activePlayer).textContent = dice;  // --> setter
-
-    //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
     document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
 
-    var x = document.querySelector('#name-1').textContent;
-    console.log(x);     // ---> getter
 
 
+document.querySelector('.btn-roll').addEventListener('click', function(){
+
+    //1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1; //+1 tam je pretože chceš nie čísla od 0-5 ale od 1-6
+
+    //2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    //3. Update the round score if number>1
+    
+})
+
+/*
+document.querySelector('#current-' + activePlayer).textContent = dice;  // --> setter
+
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+var x = document.querySelector('#name-1').textContent;
+console.log(x);     // ---> getter
+*/
