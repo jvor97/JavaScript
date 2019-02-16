750284ec029cb37f4c8ec523957bf1cbc19c6cb1
Julia Vorcakova <julia.vorcakova1@gmail.com>
2019-02-16 18:28:13 +0100

    Update round score, disapear dice, change player

--- pig game/app.js
+++ pig game/app.js
@@ -33,8 +33,23 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
+
     //3. Update the round score if number>1
-    
+    if (dice !== 1){
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }else {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        
+        document.querySelector('#current-0').textContent = roundScore;
+        document.querySelector('#current-1').textContent = roundScore;
+
+        diceDOM.style.display = 'none';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+    }
 })
 
 /*
