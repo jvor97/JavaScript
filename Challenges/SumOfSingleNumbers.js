438fbeb54ab865713947dbb30876fee859770256
JozefR <randjakjozef@gmail.com>
2019-02-17 17:45:40 +0100

    add SumOfSingleNumbers challenge

---
+++ Challenges/SumOfSingleNumbers.js
@@ -0,0 +1,8 @@
+// You get an array of numbers for example [1,3,4,3,5,4]
+// and your task is to sum numbers that occur only once.
+// [1,3,4,3,5,4] => 1, 5 => sum => 6.
+
+// Hint: use dictionary
+
+let array = [1,3,4,3,5,4];
+
