290276c24226159849302b29b3b8b38a1ca28b73
Jozef Randjak <randjakj@bluepartners.cz>
2019-02-27 19:59:28 +0100

    solution for SumOfSingleNumbers challenge

--- Challenges/SumOfSingleNumbers.js
+++ Challenges/SumOfSingleNumbers.js
@@ -2,7 +2,30 @@
 // and your task is to sum numbers that occur only once.
 // [1,3,4,3,5,4] => 1, 5 => sum => 6.
 
-// Hint: use dictionary
-
 let array = [1,3,4,3,5,4];
 
+
+let solution = SingleDigitSum(array);
+
+function SingleDigitSum(array) {
+    let dictionary = {};
+
+    for (let i = 0; i < array.length; i++){
+        if (dictionary[array[i]] === undefined) {
+            dictionary[array[i]] = 1;
+        }
+        else{
+            dictionary[array[i]] += 1;
+        }
+    }
+
+    let sum = 0;
+
+    for (let j = 0; j < array.length; j++) {
+        if (dictionary[array[j]] === 1){
+            sum += array[j];
+        }
+    }
+
+    return sum;
+}
